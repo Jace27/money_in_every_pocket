80eec2c61368764be8ba57ac2977854c6ba0fa9a
pr1-21-shilovda <pr1-21-shilovda@K21503N03>
2021-02-27 10:13:55 +0500

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MoneyInEveryPocket.Properties;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace MoneyInEveryPocket
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StaticInfo.MainWindow = this;
+            StaticInfo.MainFrame = this.MainFrame;
+            StaticInfo.MainFrame.Navigate(new Uri("Pages/Main.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ StaticInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace MoneyInEveryPocket
+{
+    public static class StaticInfo
+    {
+        public static MainWindow MainWindow;
+        public static Frame MainFrame;
+        public static Page CurrentPage;
+
+        public static string EntityConnectionString = "metadata=res://*/AwfulDB.csdl|res://*/AwfulDB.ssdl|res://*/AwfulDB.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=(localdb);initial catalog=shilov_money_in_every_pocket;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+        public static string DatabaseConnectionString = "Data Source=(localdb);Initial Catalog=shilov_money_in_every_pocket;Integrated Security=True";
+    }
+}
